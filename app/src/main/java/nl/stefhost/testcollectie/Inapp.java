d1f7a703c75e56a21ec1f64abb81a7cd969a0b1d
Stefan Vermunt <stefanvermunt@gmail.com>
2017-10-24 00:50:44 +0200

    24-10-2017 00:50

--- app/src/main/java/nl/stefhost/testcollectie/Inapp.java
+++ app/src/main/java/nl/stefhost/testcollectie/Inapp.java
@@ -1,13 +1,100 @@
 package nl.stefhost.testcollectie;
 
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.android.vending.billing.IInAppBillingService;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Inapp extends AppCompatActivity {
+
+    IInAppBillingService mService;
+
+    ServiceConnection mServiceConn = new ServiceConnection() {
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mService = null;
+        }
+
+        @Override
+        public void onServiceConnected(ComponentName name,
+                                       IBinder service) {
+            mService = IInAppBillingService.Stub.asInterface(service);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inapp);
+
+        Intent serviceIntent =
+                new Intent("com.android.vending.billing.InAppBillingService.BIND");
+        serviceIntent.setPackage("com.android.vending");
+        bindService(serviceIntent, mServiceConn, Context.BIND_AUTO_CREATE);
+
     }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mService != null) {
+            unbindService(mServiceConn);
+        }
+    }
+
+    public void kopen(View view){
+        Bundle buyIntentBundle = null;
+
+        try {
+            buyIntentBundle = mService.getBuyIntent(3, getPackageName(),
+                    "android.test.purchased", "inapp", "bGoa+V7g/yqDXvKRqq+JTFn4uQZbPiQJo4pf9RzJ");
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+
+        PendingIntent pendingIntent = buyIntentBundle.getParcelable("BUY_INTENT");
+
+        try {
+            startIntentSenderForResult(pendingIntent.getIntentSender(),
+                    1001, new Intent(), Integer.valueOf(0), Integer.valueOf(0),
+                    Integer.valueOf(0));
+        } catch (IntentSender.SendIntentException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1001) {
+            int responseCode = data.getIntExtra("RESPONSE_CODE", 0);
+            String purchaseData = data.getStringExtra("INAPP_PURCHASE_DATA");
+            String dataSignature = data.getStringExtra("INAPP_DATA_SIGNATURE");
+
+            if (resultCode == RESULT_OK) {
+                try {
+                    JSONObject jo = new JSONObject(purchaseData);
+                    String sku = jo.getString("productId");
+                    Log.d("TestCollectie", "You have bought the " + sku + ". Excellent choice, adventurer!");
+                }
+                catch (JSONException e) {
+                    Log.d("TestCollectie", "Failed to parse purchase data.");
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
